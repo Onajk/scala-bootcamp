49beb678daaadb274b47006fd07d942317a5740d
Onajk <pawel.k15@gmail.com>
2022-04-09 22:20:55 +0200

    Update Functions.scala

--- src/main/scala/com/evolutiongaming/bootcamp/functions/Functions.scala
+++ src/main/scala/com/evolutiongaming/bootcamp/functions/Functions.scala
@@ -34,13 +34,13 @@ object Functions {
   def processText2(message: String, f: String => String): String = f(message)
 
   // Exercise. Implement `isEven` method that checks if a number is even.
-  def isEven(n: Int): Boolean = ???
+  def isEven(n: Int): Boolean = n % 2 == 0
 
   // Exercise. Implement `isEvenFunc` function that behaves exactly like `isEven` method.
-  val isEvenFunc: Int => Boolean = n => ???
+  val isEvenFunc: Int => Boolean = n => n % 2 == 0
 
   // Exercise. Implement `isEvenMethodToFunc` function by transforming `isEven` method into a function.
-  val isEvenMethodToFunc: Int => Boolean = n => ???
+  val isEvenMethodToFunc: Int => Boolean = n => isEven(n)
 
   // There are traits in Scala to represent functions with various numbers of arguments: `Function0`,
   // `Function1`, `Function2`, etc. So `(A => B)` is the same as `Function1[A, B]`. A trait, where
