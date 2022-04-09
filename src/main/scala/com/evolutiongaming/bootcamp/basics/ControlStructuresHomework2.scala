1c8dce7ec6cf2a6b09200085fc8610bbab9d2b4a
Onajk <pawel.k15@gmail.com>
2022-04-09 21:31:33 +0200

    Update ControlStructuresHomework2.scala

--- src/main/scala/com/evolutiongaming/bootcamp/basics/ControlStructuresHomework2.scala
+++ src/main/scala/com/evolutiongaming/bootcamp/basics/ControlStructuresHomework2.scala
@@ -45,22 +45,46 @@ object ControlStructuresHomework2 {
   final case class ChangeMe(value: String) extends Result
 
   def parseCommand(x: String): Either[ErrorMessage, Command] = {
-    ??? // implement this method
+    // implement this method
     // Implementation hints:
     // You can use String#split, convert to List using .toList, then pattern match on:
     //   case x :: xs => ???
 
     // Consider how to handle extra whitespace gracefully (without errors).
+
+    x.split(" ").toList match {
+      case Nil => Left(ErrorMessage("Empty input."))
+      case x :: xs => x match {
+        case "divide" => xs.map(_.toDouble) match {
+          case dividend :: divisor :: Nil => Right(Command.Divide(dividend, divisor))
+          case _ => Left(ErrorMessage("Wrong number of inputs."))
+        }
+        case "sum" => Right(Command.Sum(xs.map(_.toDouble)))
+        case "average" => Right(Command.Average(xs.map(_.toDouble)))
+        case "min" => Right(Command.Min(xs.map(_.toDouble)))
+        case "max" => Right(Command.Max(xs.map(_.toDouble)))
+        case _ => Left(ErrorMessage("No such command."))
+      }
+    }
   }
 
   // should return an error (using `Left` channel) in case of division by zero and other
   // invalid operations
   def calculate(x: Command): Either[ErrorMessage, Result] = {
-    ??? // implement this method
+    x match {
+      case Command.Divide(_, divisor) if divisor == 0 => Left(ErrorMessage("Division by zero."))
+      case Command.Divide(dividend, divisor) => Right(ChangeMe(s"$dividend divided by $divisor is ${dividend/divisor}"))
+      case Command.Sum(numbers) => Right(ChangeMe(s"the sum of ${numbers.mkString(" ")} is ${numbers.sum}"))
+      case Command.Average(numbers) => Right(ChangeMe(s"the average of ${numbers.mkString(" ")} is ${numbers.sum/numbers.length}"))
+      case Command.Min(numbers) => Right(ChangeMe(s"the minimum of ${numbers.mkString(" ")} is ${numbers.min}"))
+      case Command.Max(numbers) => Right(ChangeMe(s"the maximum of ${numbers.mkString(" ")} is ${numbers.max}"))
+    }
   }
 
   def renderResult(x: Result): String = {
-    ??? // implement this method
+    x match {
+      case ChangeMe(s) => s
+    }
   }
 
   def process(x: String): String = {
@@ -69,7 +93,15 @@ object ControlStructuresHomework2 {
     // (map over the Left channel) and `merge` (convert `Either[A, A]` into `A`),
     // but you can also avoid using them using pattern matching.
 
-    ??? // implement using a for-comprehension
+    // implement using a for-comprehension
+
+    parseCommand(x) match {
+      case Left(ErrorMessage(e)) => e
+      case Right(command) => calculate(command) match {
+        case Left(ErrorMessage(e)) => e
+        case Right(result) => renderResult(result)
+      }
+    }
   }
 
   // This `main` method reads lines from stdin, passes each to `process` and outputs the return value to stdout
