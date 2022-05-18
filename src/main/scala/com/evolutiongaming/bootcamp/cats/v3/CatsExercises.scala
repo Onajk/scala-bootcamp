7b57b04e01105aa6c82f89d94f65898e76a1dc57
Onajk <pawel.k15@gmail.com>
2022-05-18 21:48:04 +0200

    Update CatsExercises.scala
    Almost done

--- src/main/scala/com/evolutiongaming/bootcamp/cats/v3/CatsExercises.scala
+++ src/main/scala/com/evolutiongaming/bootcamp/cats/v3/CatsExercises.scala
@@ -17,7 +17,7 @@ object CatsExercises {
 
       // Implement map2 here, don't use F.map2
       // Hint: f.curried will help, multiple uses of ap may be needed
-      override def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = ???
+      override def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = F.ap(F.ap(pure(f.curried))(fa))(fb)
     }
   }
 
@@ -33,14 +33,17 @@ object CatsExercises {
   }
 
   // Implement Functor's map with Applicative (ap, pure)
-  def mapFromApplicative[F[_]: Applicative, A, B](fa: F[A])(f: A => B): F[B] = ???
+  def mapFromApplicative[F[_]: Applicative, A, B](fa: F[A])(f: A => B): F[B] = Applicative[F].ap(Applicative[F].pure(f))(fa)
 
   // Implement Functor's map with Monad (flatMap, pure)
-  def mapFromMonad[F[_]: Monad, A, B](fa: F[A])(f: A => B): F[B] = ???
+  def mapFromMonad[F[_]: Monad, A, B](fa: F[A])(f: A => B): F[B] = Monad[F].flatMap(fa)(Monad[F].pure(_)).map(f)
 
   // Implement ap with Monad (flatMap, pure, map)
-  def apMonad[F[_]: Monad, A, B](ff: F[A => B])(fa: F[A]): F[B] = ???
+  def apMonad[F[_]: Monad, A, B](ff: F[A => B])(fa: F[A]): F[B] = Monad[F].flatMap(ff)(Monad[F].map(fa)(_))
 
   // Implement OptionT flatMap, for any F with a Monad.
-  def optionFlatMap[F[_]: Monad, A, B](fa: F[Option[A]])(f: A => F[Option[B]]): F[Option[B]] = ???
+  def optionFlatMap[F[_]: Monad, A, B](fa: F[Option[A]])(f: A => F[Option[B]]): F[Option[B]] = fa.flatMap {
+    case Some(a) => Monad[F].flatMap(Monad[F].pure(a))(f)
+    case None => Monad[F].pure(None)
+  }
 }
